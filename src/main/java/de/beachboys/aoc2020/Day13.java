59d8d7fa4fc1ba4f88816150c91945d7d053ca57
endre.baka <baka.endre@gmail.com>
2020-12-21 23:48:58 +0100

    13 part1

--- src/main/java/de/beachboys/aoc2020/Day13.java
+++ src/main/java/de/beachboys/aoc2020/Day13.java
@@ -2,12 +2,26 @@ package de.beachboys.aoc2020;
 
 import de.beachboys.Day;
 
+import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Day13 extends Day {
 
     public Object part1(List<String> input) {
-        return "part1result";
+        int depart = Integer.valueOf(input.get(0));
+        List<Integer> busIds = Arrays.stream(input.get(1).replaceAll("x,", "").split(",")).map(Integer::valueOf).collect(Collectors.toCollection(LinkedList::new));
+        int min = depart;
+        int minId = 0;
+        for (int id : busIds) {
+            int dif = id - (depart % id);
+            if (dif < min) {
+                min = dif;
+                minId = id;
+            }
+        }
+        return min * minId;
     }
 
     public Object part2(List<String> input) {
