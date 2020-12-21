cc28d933e93819b188fdfc1719c9a360b66beecc
endre.baka <endre.baka@casrd.net>
2020-12-21 16:17:38 +0100

    day18 par1 overflow fixed

--- src/main/java/de/beachboys/aoc2020/Day18.java
+++ src/main/java/de/beachboys/aoc2020/Day18.java
@@ -8,7 +8,7 @@ public class Day18 extends Day {
 
     private long eval(String expr) {
         String[] items = expr.split("\\s");
-        int result = Integer.parseInt(items[0]);
+        long result = Integer.parseInt(items[0]);
         for (int i = 1; i < items.length; i += 2) {
             if (items[i].equals("+")) {
                 result += Integer.parseInt(items[i+1]);
@@ -41,19 +41,19 @@ public class Day18 extends Day {
         return evalParenth(newExpr);
     }
 
-    // túl kevés 15620131092
     public Object part1(List<String> input) {
-        System.out.println(evalParenth("1 + 2 * 3 + 4 * 5 + 6"));
-        System.out.println(evalParenth("1 + (2 * 3) + (4 * (5 + 6))"));
-        System.out.println(evalParenth("2 * 3 + (4 * 5)"));
-        System.out.println(evalParenth("5 + (8 * 3 + 9 + 3 * 4 * 3)"));
-        System.out.println(evalParenth("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))"));
-        System.out.println(evalParenth("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2"));
+//        System.out.println(evalParenth("8 + (4 + 2 * 2 * 2 + 5) + 2 + (7 * (7 + 7 + 7 + 7) + 7 + 2) * ((8 + 9 * 6 + 6 * 5) + 6 + 7 * 4 * (3 + 8 * 6 + 5 + 3 * 6) * 7) * 5"));
+//        System.out.println(evalParenth("1 + 2 * 3 + 4 * 5 + 6"));
+//        System.out.println(evalParenth("1 + (2 * 3) + (4 * (5 + 6))"));
+//        System.out.println(evalParenth("2 * 3 + (4 * 5)"));
+//        System.out.println(evalParenth("5 + (8 * 3 + 9 + 3 * 4 * 3)"));
+//        System.out.println(evalParenth("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))"));
+//        System.out.println(evalParenth("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2"));
 
         long sum = 0;
         for (String s : input) {
             sum += evalParenth(s);
-            System.out.println(sum);
+            System.out.println(evalParenth(s));
         }
 
 
