96f233a3ec59cd63b7803df846ddc02fc7c6ff86
endre.baka <endre.baka@casrd.net>
2020-12-21 16:11:02 +0100

    day18 par1 implemented but gives wrong result

--- src/main/java/de/beachboys/aoc2020/Day18.java
+++ src/main/java/de/beachboys/aoc2020/Day18.java
@@ -6,8 +6,58 @@ import java.util.List;
 
 public class Day18 extends Day {
 
+    private long eval(String expr) {
+        String[] items = expr.split("\\s");
+        int result = Integer.parseInt(items[0]);
+        for (int i = 1; i < items.length; i += 2) {
+            if (items[i].equals("+")) {
+                result += Integer.parseInt(items[i+1]);
+            } else if (items[i].equals("*")) {
+                result *= Integer.parseInt(items[i+1]);
+            }
+        }
+        return result;
+    }
+
+    private long evalParenth(String expr) {
+        if (!expr.contains("(")) {
+            return eval(expr);
+        }
+
+        StringBuilder sb = new StringBuilder(expr);
+        int openIndex = 0;
+        int closeIndex = expr.length();
+        for (int i = 0; i < expr.length(); i++) {
+            if (expr.charAt(i) == '(') {
+                openIndex = i;
+            } else if (expr.charAt(i) == ')') {
+                closeIndex = i;
+                break;
+            }
+        }
+        String parenthesed = expr.substring(openIndex, closeIndex+1);
+        long newValue = eval(parenthesed.substring(1, parenthesed.length()-1));
+        String newExpr = expr.replace(parenthesed, Long.toString(newValue));
+        return evalParenth(newExpr);
+    }
+
+    // túl kevés 15620131092
     public Object part1(List<String> input) {
-        return "part1result";
+        System.out.println(evalParenth("1 + 2 * 3 + 4 * 5 + 6"));
+        System.out.println(evalParenth("1 + (2 * 3) + (4 * (5 + 6))"));
+        System.out.println(evalParenth("2 * 3 + (4 * 5)"));
+        System.out.println(evalParenth("5 + (8 * 3 + 9 + 3 * 4 * 3)"));
+        System.out.println(evalParenth("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))"));
+        System.out.println(evalParenth("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2"));
+
+        long sum = 0;
+        for (String s : input) {
+            sum += evalParenth(s);
+            System.out.println(sum);
+        }
+
+
+        return sum;
     }
 
     public Object part2(List<String> input) {
